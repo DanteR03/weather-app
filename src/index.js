6dab7fa25e939f717bb5d45bcc823516fe97f3ee
Lev <101640837+DanteR03@users.noreply.github.com.>
2025-07-30 18:25:26 +0500

    Create function to parse weather data from API and log object with required data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
+import { fetchWeatherData } from "./weatherApiInteraction.js";
 
+async function logData(location) {
+  try {
+    const data = await fetchWeatherData(location);
+    const dataJSON = await data.json();
+    const city = dataJSON.address;
+    const temp = dataJSON.currentConditions.temp;
+    const conditions = dataJSON.currentConditions.conditions;
+    const feelslike = dataJSON.currentConditions.feelslike;
+    const windspeed = dataJSON.currentConditions.windspeed;
+    const humidity = dataJSON.currentConditions.humidity;
+    console.log({ city, temp, conditions, feelslike, windspeed, humidity });
+  } catch (err) {
+    console.log("Bad location");
+  }
+}
+
+logData("London");
+logData("Seoul");
+logData("Moscow");
+logData("Bangkok");
+logData("ksdfgjksfdhgksdfjgksdfjg");
--- src/weatherApiInteraction.js
+++ src/weatherApiInteraction.js
@@ -1,6 +1,8 @@
 export function fetchWeatherData(location) {
-    const apiKey = "XECHBMMPSUW4ANN83WW8HLR8Y";
-    const weatherData = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${apiKey}`, {mode:"cors"}).then((response) => response.json());
-    return weatherData;
+  const apiKey = "XECHBMMPSUW4ANN83WW8HLR8Y";
+  const weatherData = fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${apiKey}`,
+    { mode: "cors" },
+  );
+  return weatherData;
 }
-
